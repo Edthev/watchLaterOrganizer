f2f9c88e297e120789ebb6a52b13621250d3ff08
Edward Vargas <edthevargas@gmail.com>
2024-08-05 18:58:10 -0400

    added fetchPlaylist function

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,27 @@
 const express = require("express");
+const axios = require("axios");
 const app = express();
 const port = 3001;
+
+const googleApi = "https://www.googleapis.com/youtube/v3/";
+
+const fetchPlaylists = async () => {
+   try {
+      const res = await axios
+         .get("https://www.googleapis.com/youtube/v3/playlists", {
+            params: {
+               part: "snippet",
+               mine: true,
+               key: "AIzaSyAp3HayKNaKonG5mbmC5Up2q5CCLxl8oPI",
+            },
+         })
+         .then((res) => {
+            console.log(res.data);
+         });
+   } catch (err) {
+      console.error("Err:", err);
+   }
+};
 
 app.get("/", (req, res) => {
    res.send("Hello World!");
@@ -8,4 +29,5 @@ app.get("/", (req, res) => {
 
 app.listen(port, () => {
    console.log("Connected to Server");
+   fetchPlaylists();
 });
