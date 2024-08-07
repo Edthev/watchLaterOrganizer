56c78edc4e20e825b8cbc3300c9e9c7addff7e8e
Edward Vargas <edthevargas@gmail.com>
2024-08-07 09:04:21 -0400

    added login endpoint and removed unnecesssary code

--- backend/server.js
+++ backend/server.js
@@ -2,32 +2,16 @@ const express = require("express");
 const axios = require("axios");
 const app = express();
 const port = 3001;
-
-const googleApi = "https://www.googleapis.com/youtube/v3/";
-
-const fetchPlaylists = async () => {
-   try {
-      const res = await axios
-         .get("https://www.googleapis.com/youtube/v3/playlists", {
-            params: {
-               part: "snippet",
-               mine: true,
-               key: "AIzaSyAp3HayKNaKonG5mbmC5Up2q5CCLxl8oPI",
-            },
-         })
-         .then((res) => {
-            console.log(res.data);
-         });
-   } catch (err) {
-      console.error("Err:", err);
-   }
-};
+const login = require("./EndpointFunctions/login");
 
 app.get("/", (req, res) => {
-   res.send("Hello World!");
+   res.send({
+      Connection: "Success",
+   });
 });
+
+app.get("/login", login);
 
 app.listen(port, () => {
    console.log("Connected to Server");
-   fetchPlaylists();
 });
