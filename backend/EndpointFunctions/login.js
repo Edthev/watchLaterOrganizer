d53767e1ac7dfb03d50ab758d464a5f63b87cfa9
Edward Vargas <edthevargas@gmail.com>
2024-08-07 10:50:58 -0400

    removed unused code

--- backend/EndpointFunctions/login.js
+++ backend/EndpointFunctions/login.js
@@ -1,10 +1,7 @@
 let clientSecrets = require("../client_secret.json");
-clientSecrets = clientSecrets.web;
-
-const client_id = clientSecrets.client_id || null;
-const client_secret = clientSecrets.client_secret || null;
-const redirect_uri = clientSecrets.redirect_uris || null;
-const auth_uri = clientSecrets.auth_uri || null;
+const client_id = clientSecrets.web.client_id || null;
+const redirect_uri = clientSecrets.web.redirect_uris || null;
+const auth_uri = clientSecrets.web.auth_uri || null;
 const scope =
    "https://www.googleapis.com/auth/youtube https://www.googleapis.com/auth/youtube.force-ssl https://www.googleapis.com/auth/youtube.readonly https://www.googleapis.com/auth/youtubepartner";
 
