eba77d7b32eee6f67337687387dde89a2a77e74b
Edward Vargas <edthevargas@gmail.com>
2024-08-18 15:37:42 -0400

    added authenticated get point

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,7 @@ const axios = require("axios");
 const app = express();
 const port = 3001;
 const login = require("./EndpointFunctions/login");
+const authenticated = require("./EndpointFunctions/authenticated");
 
 app.get("/", (req, res) => {
    res.send({
@@ -11,6 +12,8 @@ app.get("/", (req, res) => {
 });
 
 app.get("/login", login);
+
+app.get("/authenticated", authenticated);
 
 app.listen(port, () => {
    console.log("Connected to Server");
